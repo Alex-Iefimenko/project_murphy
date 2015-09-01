ae03ba35e414bca44b8a5a6f2d983c6b61c990f9
oefimenko <oefimenko@oefimenkos-MacBook-Pro.local>
2015-09-01 17:50:52 +0300

    Added test for animation

---
+++ Assets/Scripts/Testing/AnimationTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class AnimationTest : MonoBehaviour {
+
+	public enum Sides{ Down, Left, Right, Up };
+	public Sides currentSide = Sides.Up;
+	private Animator cAnimator;
+ 	public enum Animation{ Idle, Walk, Repair, Extinguish, Iteract, Punch, Shot, Sleep, Pull, Unconscious, Dead };
+	public Animation currentAnimation = Animation.Idle;
+
+	private RuntimeAnimatorController[] controllers = new RuntimeAnimatorController[4];
+
+	void Awake ()  {
+		// Get Door collider object
+		cAnimator   = gameObject.GetComponent<Animator>();
+		controllers = Resources.LoadAll ("NPC/Crew/").Cast<RuntimeAnimatorController>().ToArray();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		UpdateAnimator((int)currentSide);
+		cAnimator.SetInteger("State", (int)currentAnimation);
+	}
+
+	private void UpdateAnimator (int direction)
+	{
+		cAnimator.runtimeAnimatorController = controllers[direction];
+	}
+
+}
