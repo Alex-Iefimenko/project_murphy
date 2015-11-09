23b63f51c9cc98af5807b431b0be80623b01cab3
oefimenko <oefimenko@oefimenkos-MacBook-Pro.local>
2015-11-10 00:30:32 +0200

    Movements updates, enineer added

--- Assets/Scripts/Chararcter/Movement/Movement.cs
+++ Assets/Scripts/Chararcter/Movement/Movement.cs
@@ -4,22 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class Movement : MonoBehaviour, IMovement {
-
+	
 	//
 	// Variables
 	//
-
+	
 	// Character speed
 	private float speed = 0.5f;
-
+	
 	// Path components
 	private List<Vector3> movementPath = new List<Vector3>();
 	private Room currentRoom;
-
+	private bool isDynamic;
+	private GameObject target;
+	
 	// Components
 	private Collider2D charColl; 
 	private ICharacter character;
-
+	
 	//
 	// Initializing
 	//
@@ -29,71 +31,78 @@ public class Movement : MonoBehaviour, IMovement {
 		charColl = gameObject.GetComponent<Collider2D>();
 		speed = character.Stats.Speed;
 	}
-
+	
 	//
 	// Public Methods
 	//
-
+	
 	public Room CurrentRoom
 	{
 		get { return currentRoom; }
 	}
-
+	
 	public void Navigate(Room room) 
 	{
+		isDynamic = false;
+		target = room.gameObject;
 		movementPath = ShipState.Inst.GetStepsToRoom(currentRoom, room);
 		character.View.RotateTowards(movementPath[0]);
+		StopCoroutine("Adjust");
 	}
-
+	
+	public void NavigateTo(Room room, Furniture item)
+	{
+		Navigate(room);
+		target = item.gameObject;
+		movementPath[movementPath.Count - 1] = item.GetComponent<SpriteRenderer>().bounds.center;
+	}
+	
 	public void NavigateTo(Room room) 
 	{
 		Furniture item = room.GetUnoccupiedRoomObject();
 		NavigateTo(room, item);
 	}
-
-	public void NavigateTo(Room room, Furniture item)
-	{
-		Navigate(room);
-		movementPath[movementPath.Count - 1] = item.gameObject.transform.position;
-	}
-
+	
 	public void NavigateTo(ICharacter character) 
 	{
-		Debug.Log ("METHOD NOT IMPLEMENTED YET!");
+		Navigate(character.Movement.CurrentRoom);		
+		target = character.GObject;
+		isDynamic = true;
+		movementPath[movementPath.Count - 1] = target.transform.position;
 	}
 	
 	public bool IsMoving () 
 	{
 		return movementPath.Count != 0;
 	}
-
+	
 	// Check if current Char is near object
 	public bool IsNearObject (GameObject exactObject)
 	{
 		return (exactObject.collider2D != null && charColl.bounds.Intersects (exactObject.collider2D.bounds));
 	}
-
+	
 	public void Purge ()
 	{
 		movementPath.Clear();
 	}
-
+	
 	//
 	// Private Methods
 	//
-
+	
 	// Update is called once per frame
 	void Update () {
 		if (movementPath.Count != 0) Move ();
 	}
-
+	
 	// Updates current room value
 	void OnTriggerEnter2D (Collider2D otherCollider) 
 	{
 		Room room = otherCollider.GetComponent<Room>();
 		if (room != null && room != currentRoom) currentRoom = otherCollider.GetComponent<Room>();
 	}
-
+	
 	// Move character towards next movements point. Delete it if reached
 	private void Move ()
 	{
@@ -102,21 +111,28 @@ public class Movement : MonoBehaviour, IMovement {
 		transform.position = Vector3.MoveTowards(transform.position, nextPoint, speed * Time.deltaTime);
 		if (charColl.OverlapPoint(nextPoint)) UpdatePath();
 	}
-
+	
 	private void UpdatePath()
 	{
-		//{	
-		//	if (movementPath.Count == 1) AdjustPostion();
-		//}
+		if (isDynamic) NavigateTo(target.GetComponent<CharacterMain>() as ICharacter);
+		if (movementPath.Count > 1) character.View.RotateTowards (movementPath[1]);
+		if (movementPath.Count == 1) AdjustPostion();
 		movementPath.RemoveAt(0);
-		if (movementPath.Count > 0) character.View.RotateTowards (movementPath[0]);
 	}
-
+	
 	private void AdjustPostion ()
 	{
-//		Vector3 pos = (Vector3)movementPath[0];
-//		transform.position = new Vector3(pos.x, pos.y, transform.position.z);
-//		transform.rotation = endPointRotation;
+		character.View.RotateTowards (target.transform.position);
+		StartCoroutine("Adjust", movementPath[0]);
 	}
-
+	
+	IEnumerator Adjust (Vector3 endPoint)
+	{
+		while(transform.position != endPoint)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, endPoint, speed * Time.deltaTime);
+			yield return null;
+		}
+	}
 }
+
