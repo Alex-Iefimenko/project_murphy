f754c6b1a0f83fb00a27991566ba943478f39739
oefimenko <oefimenko@oefimenkos-MacBook-Pro.local>
2015-12-13 00:02:32 +0200

    captain init

---
+++ Assets/Scripts/Chararcter/AI/States/FirstAidState.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstAidState : StateBase {
+	
+	private int stateIndex = 18;
+	
+	public FirstAidState (CharacterMain character) : base(character) { }
+	
+	public override int StateKind { get { return stateIndex; } }
+	
+	public override bool CheckCondition (Room room) 
+	{
+		bool result = 
+			character.Stats.Health < character.Stats.HealthThreshold && 
+			character.Movement.CurrentRoom == ShipState.Inst.specRooms[Enums.RoomTypes.MedBay];
+		return result;
+	}
+	
+	public override void Actualize () { 
+		base.Actualize (); 
+		NavigateTo(ShipState.Inst.specRooms[Enums.RoomTypes.MedBay]);
+	}
+	
+	public override void ExecuteStateActions () 
+	{
+		if (character.Movement.IsMoving() == false)
+		{
+			character.Stats.Health += character.Stats.HealthIncrease;
+			character.Stats.HealthReduction = 0f;
+			character.View.SetSubState(1);
+		}
+		if (character.Stats.Health >= character.Stats.MaxHealth)
+			character.PurgeActions();
+	}
+	
+}
---
+++ Assets/Scripts/Chararcter/AI/States/IdleState.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IdleState : StateBase {
+	
+	private int stateIndex = 0;
+	
+	public IdleState (CharacterMain character) : base(character) { }
+	
+	public override int StateKind { get { return stateIndex; } }
+	
+	public override bool CheckCondition (Room room) 
+	{
+		return true;
+	}
+	
+	public override void Actualize () { 
+		base.Actualize (); 
+	}
+	
+	public override void ExecuteStateActions () { }
+}
